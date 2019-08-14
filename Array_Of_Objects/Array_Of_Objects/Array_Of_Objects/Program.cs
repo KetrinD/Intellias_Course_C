f5f3f5ef3168c309f179387bddd9e461cd2a24c1
Kateryna Shynkarenko <kshynkarenko@intellias.com>
2019-08-15 02:19:17 +0300

    Array_Of_Objects

---
+++ Array_Of_Objects/Array_Of_Objects/Array_Of_Objects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Of_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///8777
+
+            Table[] Table_1 = new Table[10];
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Table_1[i] = new Table(random.Next(50, 200), random.Next(50, 200));
+                Table_1[i].ShowData();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Array_Of_Objects/Array_Of_Objects/Array_Of_Objects/Tables.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Of_Objects
+{
+    class Table
+    {
+        private float width, height;
+
+        public Table()
+        {
+        }
+        public Table(float width, float height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public float Width
+        {
+            set { width = value; }
+            get { return width; }
+        }
+        public float Height
+        {
+            set { height = value; }
+            get { return height; }
+        }
+
+        public void ShowData()
+        {
+            //Console.WriteLine("Width: {0}, Heigth: {1}", width, height);  
+            // or
+            Console.WriteLine($"width: {width},  height: {height}");
+        }
+    }
+
+
+}
